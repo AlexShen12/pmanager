07b558ab842dcfaab3f5f7ee04d540422e6151f2
alshen12 <ashen006@gmail.com>
2025-02-17 19:54:30 -0500

    the beginnings of greatness

---
+++ backend/src/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    model_config: SettingsConfigDict = SettingsConfigDict(env_file= '.env', env_file_encoding='utf-8', env_ignore_empty=True)
+
+    DATABASE_URL: str
+    FERNET_KEY: bytes
+
+settings = Settings()
+
+
+
+
+
+
+
+
+
