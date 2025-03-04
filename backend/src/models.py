e8714be305e1ac0d32189dde48fd873dc7d29318
alshen12 <ashen006@gmail.com>
2025-03-03 21:39:53 -0500

    sql alchemy model done

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,34 +1,62 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from src.database.db import Base
+# built-in
+import re
+
+# external
+from sqlalchemy import Integer, String, ForeignKey 
+from sqlalchemy.orm import relationship, DeclarativeBase, mapped_column, Mapped, validates
+from sqlalchemy.schema import CheckConstraint
+
+class Base(DeclarativeBase):
+    pass
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "User"
+    id: Mapped[int] = mapped_column(Integer, primary_key = True, index = True)
+    email: Mapped[str] = mapped_column(String, unique = True)
+    username: Mapped[str] 
+    password: Mapped[str] 
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    #pwlist = Column(JSON) # Need to figure out how to validate before storing as well... Can do as dict since serialization is automatic
-
-    # linkes to stored credentials
-    credentials = relationship("Credential", back_populates= "owner", cascade= "all, delete-orphan")
-
+    __table_args__ = (
+        CheckConstraint(
+        'char_length(password) > 10', 
+        name = 'p_length_check'),
+    )
+    
+    @validates('email')
+    def validate_email(self, key, address):
+        if "@" not in address:
+            raise ValueError("Valid email address required.")
+        return address
+    
+    @validates('password')
+    def validate_password(self, key, password):
+        if not len(password) >= 10:
+            raise ValueError("Password Length must be at least 10 characters.")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+            raise ValueError("Password must contain at least one special character.")
+        return password
+    
+    credentials: Mapped[list["Credential"]] = relationship(
+        "Credential",
+        back_populates= "user",
+        cascade = "all, delete-orphan"
+    )
 
 class Credential(Base):
-    __tablename__ = "credentials"
+    __tablename__ = "Credentials"
+    id: Mapped[int] = mapped_column(Integer, primary_key =True, index = True)
+    website: Mapped[str] = mapped_column(String, index = True)
+    subusername: Mapped[str]
+    subpassword: Mapped[str]
 
-    id = Column(Integer, primary_key= True, index = True)
-    website = Column(String, index = True)
-    login = Column(String)
-    enc_pass = Column(String) 
+    user_id = Mapped[int] = mapped_column(Integer, ForeignKey('User.id'))
 
-    # To link back to user who owns this sheet. 
+    user: Mapped[list["User"]] = relationship(
+        "User",
+        back_populates="credentials"
+    )
 
-    owner_id = Column(Integer, ForeignKey('users.id'), nullable= False)
-    owner = relationship("User", back_populates="credentials")
 
-# might need to use backref? Still thinking about it.
-# Potentially can allow users to categorize passwords under different categories. 
-    
-# Is it safe to store data like this? Access to main username gives access to all... 
-    
+
+
+#Nullable or deffered look into 
